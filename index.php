e95db9c75eef8b899b72a949e972a05bc92f86dc
Yuri-Punzo <yuripunzo@hotmail.it>
2022-11-29 15:10:14 +0100

    style for form added

--- index.php
+++ index.php
@@ -28,3 +28,66 @@ ad una pagina dedicata che tramite $_SESSION (documentazione) recupererà la pas
 -->
 
 <?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Strong Password Generator</title>
+</head>
+
+<body class="bg-dark">
+
+    <h1 class="py-5 text-center text-white">Strong Password Generator</h1>
+    <!-- /header -->
+    <div class="container">
+        <div class="alert alert-primary alert-dismissible fade show" role="alert">
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            <strong>Password: </strong> Ciao
+        </div>
+        <script>
+            var alertList = document.querySelectorAll('.alert');
+            alertList.forEach(function(alert) {
+                new bootstrap.Alert(alert)
+            })
+        </script>
+    </div>
+    <!-- /answer -->
+    <div class="container bg-secondary rounded-2 p-4">
+        <form action="index.php">
+            <div class="row ">
+                <div class="col-6 ">
+                    <div>
+                        <h5>Lunghezza Password:</h5>
+                    </div>
+                    <div>
+                        <h5>Consenti ripetizione di uno o più caratteri:</h5>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div>
+                        <input type="number" name="length" id="length" class="form-control" placeholder="" aria-describedby="helpId"></label>
+                    </div>
+                </div>
+
+            </div>
+            <div class="mt-1">
+                <button type="submit" class="btn btn-primary me-2">Submit</button>
+                <button type="reset" class="btn btn-danger">Reset</button>
+            </div>
+        </form>
+    </div>
+    <!-- /form -->
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
